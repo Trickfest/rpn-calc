42cc18a778fe99bf3592ffd8991450b040b7c440
Mark Harris <noemail@specified.com>
2018-11-09 07:57:02 -0800

    Added xunit project and sln

---
+++ test/ExpressionEvalTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace rpn_calc_test
+{
+    public class ExpressionEvalTests
+    {
+        [Fact]
+        public void AlwaysSucceeds()
+        {
+            Assert.True(true);
+        }
+    }
+}
