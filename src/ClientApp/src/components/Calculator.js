f853af446f324703a9e43e6d6e55083766c79641
Mark Harris <noemail@specified.com>
2018-11-13 09:07:18 -0800

    Put the keyboard in the wrong component...oops...

--- src/ClientApp/src/components/Calculator.js
+++ src/ClientApp/src/components/Calculator.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { Display } from './Display';
+import { KeyBoard } from './KeyBoard';
 
 export class Calculator extends Component {
-  displayName = Calculator.name
+    displayName = Calculator.name
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Calculator</h1>
-        <Display/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Calculator</h1>
+                <Display />
+                <KeyBoard />
+            </div>
+        );
+    }
 }
--- src/ClientApp/src/components/Display.js
+++ src/ClientApp/src/components/Display.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import { Stack } from './Stack';
 import { InputLine } from './InputLine';
-import { KeyBoard } from './KeyBoard';
 
 export class Display extends Component {
-  displayName = Display.name
+    displayName = Display.name
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div>
-        <h2>Display</h2>
-        <Stack/>
-        <InputLine/>
-        <KeyBoard/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h2>Display</h2>
+                <Stack />
+                <InputLine />
+            </div>
+        );
+    }
 }
