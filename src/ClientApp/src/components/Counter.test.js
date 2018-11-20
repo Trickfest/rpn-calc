ee94eddd95faacc9e6d57debdfaf636772077fb6
Mark Harris <noemail@specified.com>
2018-11-20 08:57:52 -0800

    Added a test for the sample counter component

---
+++ src/ClientApp/src/components/Counter.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme';
+import Counter from './Counter';
+
+describe("Counter Suite", function () {
+
+  it("Click Test", function () {
+    configure({ adapter: new Adapter() })
+
+    var wrapper = shallow(<Counter />);
+
+    const theButton = wrapper.find('button');
+
+    theButton.simulate('click');
+    expect(wrapper.state().currentCount).toBe(1);
+
+    theButton.simulate('click');
+    expect(wrapper.state().currentCount).toBe(2);
+
+    theButton.simulate('click');
+    expect(wrapper.state().currentCount).toBe(3);
+  });
+
+});
