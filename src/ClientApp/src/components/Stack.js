52837e0a573ea3d10c6c4e368932cce515efe7ee
Mark Harris <noemail@specified.com>
2018-11-21 13:29:26 -0800

    First cut at fleshing out the stack component.

--- src/ClientApp/src/components/Stack.js
+++ src/ClientApp/src/components/Stack.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import StayScrolled from 'react-stay-scrolled';
+import StackElement from './StackElement';
 
 export class Stack extends Component {
   displayName = Stack.name
 
   constructor(props) {
     super(props);
+
+    var stackelement = { text: "123" };
+    var blankstackelement = { text: " " };
+    this.state = { stack: [blankstackelement, blankstackelement, blankstackelement, blankstackelement, blankstackelement, blankstackelement, blankstackelement, blankstackelement, blankstackelement, blankstackelement, blankstackelement, stackelement, stackelement, stackelement] };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.stackelements.length < this.props.stackelements.length)
+      this.stayScrolled(); // Or: this.scrollBottom
+  }
+
+  storeScrolledControllers = ({ stayScrolled, scrollBottom }) => {
+    this.stayScrolled = stayScrolled;
+    this.scrollBottom = scrollBottom;
   }
 
   render() {
     return (
-      <div class="stack row">
-        <p>Stack</p>
-      </div>
+      <StayScrolled provideControllers={this.storeScrolledControllers} class="row stack">
+        {
+          this.state.stack.map(
+            (stackelement, i) => <StackElement text={stackelement.text} />
+          )
+        }
+      </StayScrolled>
     );
   }
 }
---
+++ src/ClientApp/src/components/StackElement.js
@@ -0,0 +1,31 @@
+import React, { Component, propTypes } from 'react';
+import PropTypes from 'prop-types';
+import { scrolled } from 'react-stay-scrolled';
+
+//export class Counter extends Component {
+export class StackElement extends Component {
+    static propTypes = {
+        stayScrolled: PropTypes.func,
+        scrollBottom: PropTypes.func,
+    }
+
+    isNullOrEmpty(str) {
+        return !str || !str.trim();
+    }
+
+    componentDidMount() {
+        const { stayScrolled, scrollBottom } = this.props;
+
+        // Make the parent StayScrolled component scroll down if it was already scrolled
+        stayScrolled();
+
+        // Make the parent StayScrolled component scroll down, even if not completely scrolled down
+        // scrollBottom();
+    }
+
+    render() {
+        return (<div class="stackelement">{this.props.text}</div>);
+    }
+}
+
+export default scrolled(StackElement);
