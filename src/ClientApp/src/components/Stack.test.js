14463c8f45330ac957368a22c67cdd709225784e
Mark Harris <noemail@specified.com>
2018-11-21 16:09:15 -0800

    Made the reset test more robust and general code cleanup of tests.

--- src/ClientApp/src/components/Stack.test.js
+++ src/ClientApp/src/components/Stack.test.js
@@ -9,68 +9,53 @@ describe("Stack Suite", function () {
         configure({ adapter: new Adapter() })
 
         const wrapper = shallow(<Stack />);
-
         const instance = wrapper.instance();
 
         expect(instance.pop()).toBe(null);
     });
 
     it("ensures that too many pops result in a null", function () {
-        configure({ adapter: new Adapter() })
-
         const wrapper = shallow(<Stack />);
-
         const instance = wrapper.instance();
 
         instance.push(100);
-
         expect(instance.pop()).toBe(100);
-
         expect(instance.pop()).toBe(null);
     });
     
     it("ensures that a swap on an empty stack results in a null", function () {
-        configure({ adapter: new Adapter() })
-
         const wrapper = shallow(<Stack />);
-
         const instance = wrapper.instance();
 
         expect(instance.swap(200)).toBe(null);
     });
 
     it("pushes a value, pops it and verifies result", function () {
-        configure({ adapter: new Adapter() })
-
         const wrapper = shallow(<Stack />);
-
         const instance = wrapper.instance();
 
         instance.push(100);
-
         expect(instance.pop()).toBe(100);
     });
 
     it('pushes a value, swaps it and verifies the result', () => {
         const wrapper = shallow(<Stack />);
-
         const instance = wrapper.instance();
 
         instance.push(100);
-
         expect(instance.swap(200)).toBe(100);
-
         expect(instance.pop()).toBe(200);
     });
 
     
     it('ensures that a reset of a non-empty stack results in an empty stack', () => {
         const wrapper = shallow(<Stack />);
-
         const instance = wrapper.instance();
 
         instance.push(100);
         instance.push(200);
+        instance.push(300);
+        expect(instance.pop()).toBe(300);
         instance.reset();
         expect(instance.pop()).toBe(null);
     });
