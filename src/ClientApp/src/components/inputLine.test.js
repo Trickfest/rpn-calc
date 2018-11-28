64591b2aba8d7d5d769803163cbc9f16efa4ffef
Harris <markA.harris@gbrx.com>
2018-11-28 12:01:46 -0800

    Added another InputLine test.

--- src/ClientApp/src/components/inputLine.test.js
+++ src/ClientApp/src/components/inputLine.test.js
@@ -14,4 +14,17 @@ describe("InputLine Suite", function () {
         expect(instance.get()).toBe("0");
     });
 
+    
+    it("ensures that getting a previously set value works", function () {
+        configure({ adapter: new Adapter() })
+
+        const wrapper = shallow(<InputLine />);
+        const instance = wrapper.instance();
+
+        var value = "1234.56";
+
+        expect(instance.set(value)).toBe(value);
+        expect(instance.get()).toBe(value);
+    });
+
 });
