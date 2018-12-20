c2b0b85524cb2bf9a0c418fa5664f2009f4fd119
Harris <markA.harris@gbrx.com>
2018-12-20 10:16:02 -0800

    Expression eval api complete.

--- src/Controllers/RpnEvalController.cs
+++ src/Controllers/RpnEvalController.cs
@@ -11,7 +11,16 @@ namespace RpnCalc.Controllers
     public class RpnEvalController : ControllerBase
     {
         public const string HELP_TEXT = "this is help text";
+
         public const string SUCCESS_TEXT = "success";
+
+        public const string INVALID_OPERATOR_TEXT = "invalid operator";
+
+        public const string INVALID_EXPRESSION_TEXT = "invalid expression";
+
+        public const string DIVIDE_BY_ZERO_TEXT = "divide by zero";
+
+        public const string INCOMPLETE_EXPRESSION_TEXT = "incomplete expression - more than one number on stack";
 
         // GET api/rpneval/5/6/d
         [HttpGet("{*opStr}")]
@@ -19,9 +28,70 @@ namespace RpnCalc.Controllers
         {
             if (string.IsNullOrWhiteSpace(opStr))
             {
-                return new JsonResult(new RpnEvalResult() { message = HELP_TEXT, result = null });
+                return new JsonResult(new RpnEvalResult() { message = HELP_TEXT, answer = null });
             }
-            return null;
+
+            string[] tokens = opStr.Split('/');
+
+            Stack<decimal> stack = new Stack<decimal>();
+            decimal number;
+
+            foreach (string token in tokens)
+            {
+                if (decimal.TryParse(token, out number)) // is it a number? if so push on stack
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    // if there are not two items on the stack, then expression is invalid
+                    if (stack.Count < 2)
+                    {
+                        return new JsonResult(new RpnEvalResult() { message = INVALID_EXPRESSION_TEXT, answer = null });
+                    }
+
+                    decimal op2 = stack.Pop();
+                    decimal op1 = stack.Pop();
+
+                    switch (token)
+                    {
+                        case "a":
+                        case "+":
+                            stack.Push(op1 + op2);
+                            break;
+
+                        case "s":
+                        case "-":
+                            stack.Push(op1 - op2);
+                            break;
+
+                        case "m":
+                        case "*":
+                        case "x":
+                            stack.Push(op1 * op2);
+                            break;
+
+                        case "d":
+                            if (op2 == decimal.Zero)
+                            {
+                                return new JsonResult(new RpnEvalResult() { message = DIVIDE_BY_ZERO_TEXT, answer = null });
+                            }
+                            stack.Push(op1 / op2);
+                            break;
+
+                        default:
+                            return new JsonResult(new RpnEvalResult() { message = INVALID_OPERATOR_TEXT, answer = null });
+                    }
+                }
+            }
+
+            // not a single result on the stack - treat as error
+            if (stack.Count > 1)
+            {
+                return new JsonResult(new RpnEvalResult() { message = INCOMPLETE_EXPRESSION_TEXT, answer = null });
+            }
+
+            return new JsonResult(new RpnEvalResult() { message = SUCCESS_TEXT, answer = stack.Pop().ToString() });
         }
     }
 
@@ -29,6 +99,6 @@ namespace RpnCalc.Controllers
     {
         public string message { get; set; }
 
-        public string result { get; set; }
+        public string answer { get; set; }
     }
 }
--- tests/RpnEvalApiTests.cs
+++ tests/RpnEvalApiTests.cs
@@ -18,17 +18,17 @@ namespace RpnEvalTests
         {
             var rpnEvalController = new RpnEvalController();
 
-            var whitespaceResult = ((RpnCalc.Controllers.RpnEvalResult)rpnEvalController.Get("        ").Value.Value);
+            var whitespaceResult = ((RpnEvalResult)rpnEvalController.Get("        ").Value.Value);
             Assert.True(whitespaceResult.message == RpnCalc.Controllers.RpnEvalController.HELP_TEXT);
-            Assert.True(whitespaceResult.result == null);
+            Assert.True(whitespaceResult.answer == null);
 
-            var nullResult = ((RpnCalc.Controllers.RpnEvalResult)rpnEvalController.Get(null).Value.Value);
+            var nullResult = ((RpnEvalResult)rpnEvalController.Get(null).Value.Value);
             Assert.True(nullResult.message == RpnCalc.Controllers.RpnEvalController.HELP_TEXT);
-            Assert.True(nullResult.result == null);
+            Assert.True(nullResult.answer == null);
 
-            var emptyStringResult = ((RpnCalc.Controllers.RpnEvalResult)rpnEvalController.Get("").Value.Value);
+            var emptyStringResult = ((RpnEvalResult)rpnEvalController.Get("").Value.Value);
             Assert.True(emptyStringResult.message == RpnCalc.Controllers.RpnEvalController.HELP_TEXT);
-            Assert.True(emptyStringResult.result == null);
+            Assert.True(emptyStringResult.answer == null);
         }
 
         [Fact]
@@ -37,27 +37,99 @@ namespace RpnEvalTests
             var rpnEvalController = new RpnEvalController();
 
             {
-                var result = ((RpnCalc.Controllers.RpnEvalResult)rpnEvalController.Get("10/5/p").Value.Value);
+                var result = ((RpnEvalResult)rpnEvalController.Get("10/5/a").Value.Value);
                 Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.SUCCESS_TEXT);
-                Assert.True(double.Parse(result.result) == 15);
+                Assert.True(decimal.Parse(result.answer) == 15);
             }
 
             {
-                var result = ((RpnCalc.Controllers.RpnEvalResult)rpnEvalController.Get("10/5/s").Value.Value);
+                var result = ((RpnEvalResult)rpnEvalController.Get("10/5/s").Value.Value);
                 Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.SUCCESS_TEXT);
-                Assert.True(double.Parse(result.result) == 5);
+                Assert.True(decimal.Parse(result.answer) == 5);
             }
 
             {
-                var result = ((RpnCalc.Controllers.RpnEvalResult)rpnEvalController.Get("10/5/m").Value.Value);
+                var result = ((RpnEvalResult)rpnEvalController.Get("10/5/m").Value.Value);
                 Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.SUCCESS_TEXT);
-                Assert.True(double.Parse(result.result) == 50);
+                Assert.True(decimal.Parse(result.answer) == 50);
             }
-            
+
             {
-                var result = ((RpnCalc.Controllers.RpnEvalResult)rpnEvalController.Get("10/5/d").Value.Value);
+                var result = ((RpnEvalResult)rpnEvalController.Get("10/5/d").Value.Value);
                 Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.SUCCESS_TEXT);
-                Assert.True(double.Parse(result.result) == 2);
+                Assert.True(decimal.Parse(result.answer) == 2);
+            }
+        }
+
+        [Fact]
+        public void BadExpressionTests()
+        {
+            var rpnEvalController = new RpnEvalController();
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("10/5/p").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.INVALID_OPERATOR_TEXT);
+                Assert.True(result.answer == null);
+            }
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("5/+").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.INVALID_EXPRESSION_TEXT);
+                Assert.True(result.answer == null);
+            }
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("+").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.INVALID_EXPRESSION_TEXT);
+                Assert.True(result.answer == null);
+            }
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("5/5/5/a").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.INCOMPLETE_EXPRESSION_TEXT);
+                Assert.True(result.answer == null);
+            }
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("5/0/d").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.DIVIDE_BY_ZERO_TEXT);
+                Assert.True(result.answer == null);
+            }
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("5/4/4/s/d").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.DIVIDE_BY_ZERO_TEXT);
+                Assert.True(result.answer == null);
+            }
+        }
+
+        [Fact]
+        public void ComplexExpressionTests()
+        {
+            var rpnEvalController = new RpnEvalController();
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("10/3/d").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.SUCCESS_TEXT);
+                Assert.True(decimal.Parse(result.answer) == decimal.Parse(((decimal)10 / (decimal)3).ToString()));
+            }
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("-10/-3/a").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.SUCCESS_TEXT);
+                Assert.True(decimal.Parse(result.answer) == -13);
+            }
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("-10.4/-3.7/a").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.SUCCESS_TEXT);
+                Assert.True(decimal.Parse(result.answer) == (decimal)-14.1);
+            }
+
+            {
+                var result = ((RpnEvalResult)rpnEvalController.Get("-10.4/-3.7/a/4/s/2/*/04.00/2/d/d/2/d/5.0/0002.000/*/x/00.007/-/-00005.50000/+").Value.Value);
+                Assert.True(result.message == RpnCalc.Controllers.RpnEvalController.SUCCESS_TEXT);
+                Assert.True(decimal.Parse(result.answer) == (decimal)-96.007);
             }
         }
     }
