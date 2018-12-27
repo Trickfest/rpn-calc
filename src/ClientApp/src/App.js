a7b9a13b8668b357ea579657772c31f15acc1958
Harris <markA.harris@gbrx.com>
2018-12-27 14:56:28 -0800

    Removed counter and fetchdata pages and components.

--- src/ClientApp/src/App.js
+++ src/ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { Calculator } from './components/Calculator';
 
 export default class App extends Component {
@@ -14,8 +12,6 @@ export default class App extends Component {
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/calculator' component={Calculator} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
       </Layout>
     );
   }
